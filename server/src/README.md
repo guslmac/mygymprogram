84948bf61e3ef596ac906a4d2de5c935f5e4d687
Gustavo Laranjeira Machado <guslmac@gmail.com>
2020-03-21 11:50:04 -0300

    First commit

---
+++ server/src/README.md
@@ -0,0 +1,2 @@
+My Gym Program is a simple app to register my workout exrcises, with it's descriptions and weights.
+It's the porpouse to develop my skills in Node.js, mongoDB and GraphQL
---
+++ server/src/config/databaseConnection.js
@@ -0,0 +1,27 @@
+const MongoClient = require('mongodb').MongoClient;
+
+let mongoDB;
+
+const setupDB = callback => {
+  const uri = 'mongodb://heroku_6b3qf1n0:heroku_6b3qf1n0@ds157325.mlab.com:57325/heroku_6b3qf1n0'
+  
+  MongoClient.connect(
+    uri,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+      mongoDB = client.db('heroku_6b3qf1n0');
+
+      if (err) {
+        return callback(err);
+      } else {
+        return callback('DB OK');
+      }
+    }
+  );
+};
+
+const getDB = () => {
+  return mongoDB;
+};
+
+module.exports = { setupDB, getDB };
